ecda75ce260f47505e4668b0abfb3197c3083cfa
Yeshodha S <165245112+yeshodha02@users.noreply.github.com>
2025-04-07 09:28:32 +0530

    Update ExceptionThrower.java

--- ExceptionThrower.java
+++ ExceptionThrower.java
@@ -2,7 +2,6 @@ package com.edurekademo.utilities;
 
 import java.io.IOException;
 
-import com.edurekademo.utilities.LoggerStackTraceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
